5b4cca622b6ec8bef653b7a9c12ccae851089959
Qiang Liu <cyruscyliu@gmail.com>
2022-01-08 20:04:52 +0100

    update nyx to profile scripts

---
+++ M1.sh
@@ -0,0 +1,5 @@
+# parallel -j 32 --bar < /tmp/workdir_qemu-legacy_ac97-.sh
+parallel -j 32 --bar < /tmp/workdir_qemu-legacy_cs4231a-.sh
+# parallel -j 32 --bar < /tmp/workdir_qemu-legacy_sdhci-.sh
+parallel -j 32 --bar < /tmp/workdir_qemu-legacy_ee100pro-.sh
+parallel -j 32 --bar < /tmp/workdir_qemu-legacy_e1000-.sh
---
+++ M2.sh
@@ -0,0 +1,5 @@
+parallel -j 32 --bar < /tmp/workdir_qemu-legacy_intel_hda-.sh
+# parallel -j 32 --bar < /tmp/workdir_qemu-legacy_parallel-.sh
+parallel -j 32 --bar < /tmp/workdir_qemu-legacy_ne2000-.sh
+# parallel -j 32 --bar < /tmp/workdir_qemu-legacy_sb16-.sh
+# parallel -j 32 --bar < /tmp/workdir_qemu-legacy_pcnet-.sh
