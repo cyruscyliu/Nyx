01f44a853a1663f3f61e4941eab2ac6915b51814
Qiang Liu <cyruscyliu@gmail.com>
2022-09-13 20:52:48 +0800

    enable merge parallel

--- 13-merge-parallel.sh
+++ 13-merge-parallel.sh
@@ -6,6 +6,6 @@ mkdir $merged_src
 
 for i in {0..9}; do
     python3 13-merge-parallel.py $orig_src $merged_src ${i} > /tmp/$(basename $orig_src).merged-${i}.sh
-    # bash -x /tmp/$(basename $orig_src).merged-${i}.sh &
+    bash -x /tmp/$(basename $orig_src).merged-${i}.sh &
 done
 
